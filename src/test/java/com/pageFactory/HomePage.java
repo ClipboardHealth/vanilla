e5d6e0bce479f44b000225edd453ba32a5854885
codelikeaddy <108233413+codelikeaddy@users.noreply.github.com>
2022-06-26 14:03:39 -0400

    Add some more test steps and helper functions

--- src/test/java/com/pageFactory/HomePage.java
+++ src/test/java/com/pageFactory/HomePage.java
@@ -7,6 +7,28 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends TestBase implements Constants {
 
+    public void clickOnElement(String elementXpath) {
+        WebElement element = driver.findElement(By.xpath(elementXpath));
+        waitForElementToBeVisible(element);
+        element.click();
+    }
+
+    public void scrollToElement(String elementXpath) {
+        WebElement elementToFocus = driver.findElement(By.xpath(elementXpath));
+        je.executeScript("arguments[0].scrollIntoView(true);", elementToFocus);
+    }
+
+    public void waitForElementToBeClickable(String elementXpath) {
+        WebElement element1 = driver.findElement(By.xpath(elementXpath));
+        wait.until(ExpectedConditions.elementToBeClickable(element1));
+    }
+
+    public void switchToWindow() {
+        for (String winHandle : driver.getWindowHandles()) {
+            driver.switchTo().window(winHandle);
+        }
+    }
+
     public void verifyElementIsPresent(String elementXpath) {
         WebElement element = driver.findElement(By.xpath(elementXpath));
         waitForElementToBeVisible(element);
--- src/test/java/com/testCases/Test1.java
+++ src/test/java/com/testCases/Test1.java
@@ -17,6 +17,27 @@ public class Test1 extends HomePage {
         driver.navigate().to("https://www.amazon.in/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         hp.verifyElementIsPresent(Xpath_Todays_Deals);
+        //Step2: Click on the hamburger menu in the top left corner
+        hp.clickOnElement(Xpath_Hamburger_Menu);
+        //Step3: Scroll own and then Click on the TV, Appliances and Electronics link under Shop by Department section
+        hp.clickOnElement(Xpath_TvAppliancesDept_Link);
+        //Step 4: Then click on Televisions under Tv, Audio & Cameras sub-section
+        hp.clickOnElement(Xpath_Televisions);
+        //Step 5: Scroll down and filter the results by Brand ‘Samsung
+        hp.scrollToElement(Xpath_Samsung_Branch_Checkbox);
+        hp.waitForElementToBeClickable(Xpath_Samsung_Branch_Checkbox);
+        Thread.sleep(3000);
+        hp.clickOnElement(Xpath_Samsung_Branch_Checkbox);
+        //Step 6: Sort the Samsung results with price High to Low
+        hp.waitForElementToBeClickable(Xpath_Sort_Items_Price);
+        hp.clickOnElement(Xpath_Sort_Items_Price);
+        Thread.sleep(2000);
+        hp.clickOnElement(Xpath_Sort_Price_LowToHigh);
+        // Store the current window handle
+        String winHandleBefore = driver.getWindowHandle();
+        //Step 7:Click on the second highest priced item
+        hp.clickOnElement(Xpath_Second_Result_Item);
+        hp.switchToWindow();
         closeBrowser();
     }
 
